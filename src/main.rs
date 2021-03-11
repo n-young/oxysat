5203b4196619f208c3d3d2bf2a3be64eb7ad58a8
Nick Young <youngnicholas228@gmail.com>
2021-03-10 23:02:16 -0500

    docs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::env;
 use std::fs;
 
+// Main!
 fn main() {
+    // Get args from user.
     let args: Vec<_> = env::args().collect();
 
+    // Check args length.
     if args.len() != 2 {
         println!("Expected 1 arugment. Usage: ./run.sh <filename>");
         panic!(1);
     }
+
+    // Get filename, read raw contents.
     let filename: String = format!("{}", args[1]);
     let contents = fs::read_to_string(filename).expect("File not found");
 
--- src/structs.rs
+++ src/structs.rs
@@ -1,14 +1,18 @@
 use std::fmt;
+
+// Sign enum - either Positive or Negative.
 pub enum Sign {
     Position,
     Negative,
 }
 
+// Literal struct - has a name and a Sign.
 pub struct Literal {
     pub name: String,
     pub sign: Sign,
 }
 
+// Formatter for a Literal.
 impl Display for Literal {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self.sign {
@@ -18,11 +22,13 @@ impl Display for Literal {
     }
 }
 
+// Clause struct - has an ID and a set of Literals.
 pub struct Clause {
     pub id: i64,
     pub literal: Vec<Literal>,
 }
 
+// Formatter for a Clause.
 impl Display for Clause {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "ID: {} Literals: {}", self.id, self.literal.join(", "))
