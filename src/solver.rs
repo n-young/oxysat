c34085527307b491d37ef0171edf43028aaba496
Paul Biberstein <biberstein.paul@gmail.com>
2021-03-10 23:01:18 -0500

    add fns defns for solver and parser

---
+++ src/solver.rs
@@ -0,0 +1,22 @@
+fn solve(clauses: Vec<Clause>) -> (Bool, Vec<Literal>) {}
+
+fn solveHelper(assignment: Vec<Literal>, clauses: Vec<Clause>) -> (Bool, Vec<Literal>) {}
+
+// Perform unit clause elimination and return resulting clauses/assignment
+fn unitClauseElimination(
+    assignment: Vec<Literal>,
+    clauses: Vec<Clause>,
+) -> (Vec<Literal>, Vec<Clause>) {
+}
+
+// Perform pure literal elimination and return resulting clauses/assignment
+fn pureLiteralElimination(
+    assignment: Vec<Literal>,
+    clauses: Vec<Clause>,
+) -> (Vec<Literal>, Vec<Clause>) {
+}
+
+fn pickVar(clauses: Vec<Clause>) -> (Vec<Literal>, Vec<Literal>, Vec<Clause>, Vec<Clause>) {
+    // TODO: pick a random element and return the resulting formula from assigning
+    // it to true/false
+}
