601ff9a48d43d8d35bf1b1d49fffb9022be8a9e2
Paul Biberstein <biberstein.paul@gmail.com>
2021-03-13 14:45:20 -0500

    Add solve_helper

--- src/solver.rs
+++ src/solver.rs
@@ -1,11 +1,36 @@
 use crate::structs::*;
 
 pub fn solve(clauses: Vec<Clause>) -> (bool, Vec<Literal>) {
-    panic!()
+    let assignment = vec![];
+    solve_helper(assignment, clauses)
 }
 
 fn solve_helper(assignment: Vec<Literal>, clauses: Vec<Clause>) -> (bool, Vec<Literal>) {
-    panic!()
+    let (assignment, clauses) = unit_clause_elimination(assignment, clauses);
+
+    let (assignment, clauses) = pure_literal_elimination(assignment, clauses);
+
+    if clauses.len() == 0 {
+        return (true, assignment);
+    }
+
+    //  if formula has an empty clause return UNSAT
+    for clause in &clauses {
+        if clause.literals.len() == 0 {
+            return (false, vec![]);
+        }
+    }
+
+    let (assignment_if_true, assignment_if_false, clauses_if_true, clauses_if_false) =
+        pick_var(clauses);
+
+    let (true_results_is_sat, assignment) = solve_helper(assignment_if_true, clauses_if_true);
+
+    if true_results_is_sat {
+        (true, assignment)
+    } else {
+        solve_helper(assignment_if_false, clauses_if_false)
+    }
 }
 
 // Perform unit clause elimination and return resulting clauses/assignment
